5b19252b54cd994262c5c8fee7a5401019a02329
Thomas Kim <thomkim@e1z3r7p6.42.us.org>
2018-08-01 09:46:38 -0700

    subjects

--- ft_atoi_base/ft_atoi_base.c
+++ ft_atoi_base/ft_atoi_base.c
@@ -1,67 +1,37 @@
-int	ft_atoi_base(const char *str, int str_base)
+int		test_base(const char c)
 {
-	int i;
-	i = 0;
+	int		nb;
 
-	int neg;
-	neg = 0;
-
-	int nbr;
-	nbr = 0;
-	while (str[i])
-	{
-		while ((str[i] >= 9 && str[i] <= 13 ) || str[i] == 32)
-			i++;
-		if (str[i] == '-')
-		{
-			while (str[i] == '-')
-				i++;
-			neg = 1;
-		}
-		if (str_base == 1 || str_base == 0)
-			return (0);
-		if (str_base <= 10)
-			nbr = (str_base * nbr) + (str[i] - '0');
-		if (str_base > 10)
-		{
-			if (str[i] == 'a')
-				nbr = (str_base * nbr) + 10;
-			else if (str[i] == 'A')
-				nbr = (str_base * nbr) + 10;
-			else if (str[i] == 'b')
-				nbr = (str_base * nbr) + 11;
-			else if (str[i] == 'B')
-				nbr = (str_base * nbr) + 11;
-			else if (str[i] == 'c')
-				nbr = (str_base * nbr) + 12;
-			else if (str[i] == 'C')
-				nbr = (str_base * nbr) + 12;
-			else if (str[i] == 'd')
-				nbr = (str_base * nbr) + 13;
-			else if (str[i] == 'D')
-				nbr = (str_base * nbr) + 13;
-			else if (str[i] == 'e')
-				nbr = (str_base * nbr) + 14;
-			else if (str[i] == 'E')
-				nbr = (str_base * nbr) + 14;
-			else if (str[i] == 'f')
-				nbr = (str_base * nbr) + 15;
-			else if (str[i] == 'F')
-				nbr = (str_base * nbr) + 15;
-			else
-				nbr = (str_base * nbr) + (str[i] - '0');
-		}
-		i++;
-	}
-	if (neg == 1)
-		nbr = nbr * (-1);
-	return (nbr);
+	if (c >= '0' && c <= '9')
+		nb = c - '0';
+	else if (c >= 'a' && c <= 'z')
+		nb = c - 'a' + 10;
+	else if (c >= 'A' && c <= 'Z')
+		nb = c - 'A' + 10;
+	else
+		nb = -1;
+	return (nb);
 }
 
-#include "stdio.h"
-int	main()
+int		ft_atoi_base(const char *str, int str_base)
 {
-	char a[10] = "--233";
-	printf("%d\n", ft_atoi_base(a, 10));
-	return 0;
+	int		neg;
+	int		nb;
+	int		current;
+
+	nb = 0;
+	while (*str == ' ' || *str == '\n' || *str == '\t'
+		|| *str == '\f' || *str == '\r' || *str == '\v')
+		str++;
+	neg = (*str == '-' ? 1 : 0);
+	if (*str == '-' || *str == '+')
+		str++;
+	current = test_base(*str);
+	while (current >= 0 && current < str_base)
+	{
+		nb = nb * str_base + current;
+		str++;
+		current = test_base(*str);
+	}
+	return (neg ? -nb : nb);
 }
